67130915f73b99e4b163462f885f526b48671859
Mikhail Kurnosov <mkurnosov@jet.cpct.sibsutis.ru>
2012-05-02 12:48:51 +0700

    Test for Ibcast is implemented

---
+++ src/nbc/ibcast.h
@@ -0,0 +1,22 @@
+/*
+ * ibcast.h: Benchmark functions for Ibcast.
+ *
+ * Copyright (C) 2012 Mikhail Kurnosov
+ */
+
+#include <mpi.h>
+
+#ifndef IBCAST_H
+#define IBCAST_H
+
+#include "bench_nbc.h"
+
+int bench_ibcast_init(nbctest_params_t *params);
+int bench_ibcast_free();
+int bench_ibcast_printinfo();
+int measure_ibcast_blocking(nbctest_params_t *params,
+		                    nbctest_result_t *result);
+int measure_ibcast_overlap(nbctest_params_t *params,
+		                   nbctest_result_t *result);
+
+#endif /* IBCAST_H */
